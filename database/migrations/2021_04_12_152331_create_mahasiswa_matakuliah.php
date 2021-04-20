9b54097401db4eeacba9817fbd2bedc8a78a76ce
tasya <tasya00661@gmail.com>
2021-04-20 22:11:19 +0700

    Migration create Mahasiswa_Matakuliah

---
+++ database/migrations/2021_04_12_152331_create_mahasiswa_matakuliah.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswaMatakuliah extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa_matakuliah', function (Blueprint $table) {
+            $table->id();
+            $table->string('mahasiswa_id', 20);
+            $table->unsignedBigInteger('matakuliah_id');
+            $table->foreign('mahasiswa_id')->references('nim')->on('mahasiswa');
+            $table->foreign('matakuliah_id')->references('id')->on('matakuliah');
+            $table->char('nilai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa_matakuliah');
+    }
+}
