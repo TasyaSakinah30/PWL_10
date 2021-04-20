5c1a7c84dfee8d4e9a119c27f49545374f7051e0
tasya <tasya00661@gmail.com>
2021-04-20 22:02:55 +0700

    Mahasiswa Controller

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+use App\Models\Kelas;
+
+class MahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //fungsi eloquent menampilkan data menggunakan pagination
+        // $mahasiswas = Mahasiswa::all(); // Mengambil semua isi tabel
+        // $posts = Mahasiswa::orderBy('nim', 'desc')->paginate(6);
+        $posts = Mahasiswa::with('kelas')->get();
+        $paginate = Mahasiswa::orderBy('nim', 'asc')->paginate(3);
+        return view('mahasiswas.index', ['mahasiswa' => $posts, 'paginate' => $paginate]);
+        // $search = request()->query('search');
+        // if($search) {
+        //     $posts = Mahasiswa::where('nama', 'LIKE', "%{$search}%")->paginate(3);
+        // } else {
+        //     $posts = Mahasiswa::orderBy('nim', 'desc')->paginate(5);
+        // }
+        // return view('mahasiswas.index', compact('posts'));
+        // with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // return view('mahasiswas.create');
+        $kelas = kelas::all(); // mendapatkan data dari tabel kelas
+        return view('mahasiswas.create',['kelas' => $kelas]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //melakukan validasi data
+        $request->validate([
+        'nim' => 'required',
+        'nama' => 'required',
+        'kelas' => 'required',
+        'jurusan' => 'required',
+        'no_handphone' => 'required',
+    ]);
+        $mahasiswa = new Mahasiswa;
+        $mahasiswa->nim = $request->get('nim');
+        $mahasiswa->nama = $request->get('nama');
+        $mahasiswa->jurusan = $request->get('jurusan');
+        $mahasiswa->no_handphone = $request->get('no_handphone');
+        // $mahasiswa->save();
+
+        $kelas = new Kelas;
+        $kelas->id = $request->get('kelas');
+
+        //fungsi eloquent untuk menambah data dengan relasi belongsTo
+        // Mahasiswa::create($request->all());
+        $mahasiswa->kelas()->associate($kelas);
+        $mahasiswa->save();
+
+        //jika data berhasil ditambahkan, akan kembali ke halaman utama
+        return redirect()->route('mahasiswa.index')
+        ->with('success', 'Mahasiswa Berhasil Ditambahkan');
+        // dd($mahasiswa);
+   
+    }
+    public function nilai($nim)
+    {
+        $mahasiswa = Mahasiswa::with('kelas', 'matakuliah')->where('nim', $nim)->first();
+        return view('mahasiswas.khs', compact('mahasiswa'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($nim)
+    {
+        //menampilkan detail data dengan menemukan/berdasarkan Nim Mahasiswa
+        // code sebelum dibuat relasi -> $Mahasiswa = Mahasiswa::find($nim);
+        // return view('mahasiswas.detail', compact('Mahasiswa'));
+        $mahasiswa = Mahasiswa::with('kelas')->where('nim', $nim)-> first();
+        return view('mahasiswas.detail', ['Mahasiswa' => $mahasiswa]);
+    }
+
+    public function cetak_pdf($nim){
+        $mahasiswa = Mahasiswa::with('kelas')->where('nim', $nim)-> first();
+        $pdf = PDF::loadview('mahasiswas.khs', compact('mahasiswa'));
+        return $pdf->stream();
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($nim)
+    {
+        //menampilkan detail data dengan menemukan berdasarkan Nim Mahasiswa untuk diedit
+        // $Mahasiswa = Mahasiswa::find($nim);
+        $mahasiswa = Mahasiswa::with('kelas')->where('nim', $nim)->first();
+        $kelas = kelas::all(); // mendapatkan data dari tabel kelas
+        return view('mahasiswas.edit', compact('mahasiswa', 'kelas'));
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $nim)
+    {
+        //melakukan validasi data
+        $request->validate([
+        'nim' => 'required',
+        'nama' => 'required',
+        'kelas' => 'required',
+        'jurusan' => 'required',
+        'no_handphone' => 'required',
+    ]);
+        $mahasiswa = Mahasiswa::with('kelas')->where('nim', $nim)->first();
+        $mahasiswa->nim = $request->get('nim');
+        $mahasiswa->nama = $request->get('nama');
+        $mahasiswa->jurusan = $request->get('jurusan');
+        $mahasiswa->no_handphone = $request->get('no_handphone');
+        $mahasiswa->save();
+
+        $kelas = new Kelas;
+        $kelas->id = $request->get('kelas');
+
+        //fungsi eloquent untuk mengupdate data inputan kita
+        // Mahasiswa::find($nim)->update($request->all());
+        $mahasiswa->kelas()->associate($kelas);
+        $mahasiswa->save();
+
+        //jika data berhasil diupdate, akan kembali ke halaman utama
+        return redirect()->route('mahasiswa.index')
+        ->with('success', 'Mahasiswa Berhasil Diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($nim)
+    {
+        //fungsi eloquent untuk menghapus data
+        Mahasiswa::find($nim)->delete();
+        return redirect()->route('mahasiswa.index')
+        -> with('success', 'Mahasiswa Berhasil Dihapus');
+
+    }
+}
